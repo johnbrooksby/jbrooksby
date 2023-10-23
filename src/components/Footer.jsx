9c81941ca9b42ade09497bdd65d56311115e140c
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-10-22 22:43:04 -0600

    Changes font size of text in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,14 @@ import React, {useState} from "react";
 const Footer = (props) => {
   return (
     <div>
-      <footer className={props.darkMode ? "foot darkMode" : "foot"}>
+      <footer className={props.darkMode ? "foot footerText darkMode" : "foot footerText"}>
         <a onClick={() => {
           props.setDarkMode(!props.darkMode)
           console.log(props.darkMode)
         }}>{props.darkMode ? "Light Mode" : "Dark Mode"}</a>
-        <p>
+        <p style={{fontSize:'14pt'}}>
           &copy; 2023 John Brooksby
         </p>
-
       </footer>
     </div>
   );

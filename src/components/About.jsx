b7ec647b3cec0d444aefc495507aa3d3d5c589ed
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-12-24 13:18:07 -0700

    Minor edits to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,11 +19,11 @@ const About = (props) => {
         About
       </h2>
       <section hidden={props.hide.about}>
-        <p>I have a good work ethic and I work well with others</p>
-        <p>I continue to show up when things get hard</p>
+        <p>I have a good work ethic and I work well with others.</p>
+        <p>I continue to show up when things get hard.</p>
         <p>I have a knack for identifying inefficiencies</p>
-        <p>and working to improve on them</p>
-        <p>I am a finisher</p>
+        <p>and working to improve on them.</p>
+        <p>I am a finisher.</p>
         <h3></h3>
       </section>
     </div>
